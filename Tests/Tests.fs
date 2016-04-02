96be39c450a95544ecf62bff0c428cf5a202ebbf
Christopher Nikkel <chris@freedomvoice.com>
2016-04-02 14:43:07 -0700

    Remove extra words

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -15,9 +15,9 @@ module Tests
     [<Property>]
     let ``draw lines`` (x1, y1, x2, y2, c, r, g, b, p) =
         configureLogs
-        let point1 = { X = Size.Pixels(x1); Y = Size.Pixels(y1) }
-        let point2 = { X = Size.Pixels(x2); Y = Size.Pixels(y2) }
-        let style = { Stroke = Color.Values(r, g, b); StrokeWidth = Pixels(p); Fill = Color.Hex(c); }
+        let point1 = { X = Pixels(x1); Y = Pixels(y1) }
+        let point2 = { X = Pixels(x2); Y = Pixels(y2) }
+        let style = { Stroke = Values(r, g, b); StrokeWidth = Pixels(p); Fill = Hex(c); }
         let line = { Point1 = point1; Point2 = point2; }
         let tagString = SvgLine(line, Some(style)).toString
 
@@ -26,9 +26,9 @@ module Tests
     [<Property>]
     let ``draw rectangles`` (x, y, h, w, c, r, g, b, p) =
         configureLogs
-        let point = { X = Size.Pixels(x); Y = Size.Pixels(y); }
-        let area = { Height = Size.Pixels(h); Width = Size.Pixels(w); }
-        let style = { Stroke = Color.Values(r, g, b); StrokeWidth = Pixels(p); Fill = Color.Hex(c); }
+        let point = { X = Pixels(x); Y = Pixels(y); }
+        let area = { Height = Pixels(h); Width = Pixels(w); }
+        let style = { Stroke = Values(r, g, b); StrokeWidth = Pixels(p); Fill = Hex(c); }
         let (rect : Rect) = { UpperLeft = point; Size = area; }
         let tagString = SvgRect(rect, Some(style)).toString
 
@@ -37,8 +37,8 @@ module Tests
     [<Property>]
     let ``draw circles`` (x, y, radius, c, r, g, b, p) =
         configureLogs
-        let point = { X = Size.Pixels(x); Y = Size.Pixels(y) }
-        let style = { Stroke = Color.Values(r, g, b); StrokeWidth = Pixels(p); Fill = Color.Hex(c); }
+        let point = { X = Pixels(x); Y = Pixels(y) }
+        let style = { Stroke = Values(r, g, b); StrokeWidth = Pixels(p); Fill = Hex(c); }
         let circle = { Center = point; Radius = radius }
         let tagString = SvgCircle(circle, Some(style)).toString
 
@@ -48,14 +48,14 @@ module Tests
     let ``do lots and don't fail`` () =
         configureLogs
         let points = seq {
-            yield {X = Size.Pixels(1); Y = Size.Pixels(1)}
-            yield {X = Size.Pixels(4); Y = Size.Pixels(4)}
-            yield {X = Size.Pixels(10); Y = Size.Pixels(10)}
+            yield {X = Pixels(1); Y = Pixels(1)}
+            yield {X = Pixels(4); Y = Pixels(4)}
+            yield {X = Pixels(10); Y = Pixels(10)}
         }
-        let point = {X = Size.Pixels(24); Y = Size.Pixels(15)}
-        let size = {Height = Size.Pixels(30); Width = Size.Pixels(30)}
-        let style1 = {Stroke = (Hex(0xff0000)); StrokeWidth = Pixels(3); Fill = Color.Name(Colors.Red); }
-        let style2 = {Stroke = (SmallHex(0xf00s)); StrokeWidth = Pixels(6); Fill = Color.Name(Colors.Blue); }
+        let point = {X = Pixels(24); Y = Pixels(15)}
+        let size = {Height = Pixels(30); Width = Pixels(30)}
+        let style1 = {Stroke = (Hex(0xff0000)); StrokeWidth = Pixels(3); Fill = Name(Colors.Red); }
+        let style2 = {Stroke = (SmallHex(0xf00s)); StrokeWidth = Pixels(6); Fill = Name(Colors.Blue); }
         let transform = Transform.Scale(2, 5)
 
         let graphics = seq {
