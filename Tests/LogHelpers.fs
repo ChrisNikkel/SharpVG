c2e51f134f250e1de2aa1f8107a47ac24c027f86
Christopher Nikkel <chris@freedomvoice.com>
2016-04-02 12:10:54 -0700

    Add log result helpers for basic checks

--- Tests/LogHelpers.fs
+++ Tests/LogHelpers.fs
@@ -3,9 +3,23 @@ module LogHelpers
     open log4net.Config
 
     let _log = LogManager.GetLogger "Tests"
+
+    let configureLogs = BasicConfigurator.Configure() |> ignore
+
     let debug format = Printf.ksprintf _log.Debug format
     let info format = Printf.ksprintf _log.Info format
     let warn format = Printf.ksprintf _log.Warn format
     let error format = Printf.ksprintf _log.Error format
     let fatal format = Printf.ksprintf _log.Fatal format
-    let configureLogs = BasicConfigurator.Configure() |> ignore
+
+    let tee f x =
+        f x
+        x
+
+    let logbool name =
+        function
+            | true -> info "%s: true" name
+            | false -> error "%s: false" name
+
+    let logResult name =
+        tee <| logbool name
