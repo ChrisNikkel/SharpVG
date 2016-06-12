4a754036b831cef11c2344a66304fe80f2714781
Christopher Nikkel <chris@immortalcreation.com>
2016-06-12 12:27:46 -0700

    Add initial animation (really)

---
+++ SharpVG/Animation.fs
@@ -0,0 +1,44 @@
+﻿namespace SharpVG
+
+open System
+
+type CalculationMode =
+    | Discrete
+    | Linear
+    | Paced
+    | Spline
+
+type Motion =
+    {
+        Path: Path
+        CalculationMode: CalculationMode option
+    }
+
+type AnimationType =
+    | Change // set
+    | Adjust // animate
+    | Transform
+    | Motion of Motion
+    | Color
+
+type Animation =
+    {
+        AnimationType: AnimationType
+        Timing: Timing
+    }
+
+[<CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
+module Animation =
+    let toTag animation =
+        let name, attribute =
+            match animation.AnimationType with 
+                | Change -> "set", ""
+                | Adjust -> "animate", ""
+                | Transform -> "animateTransform", ""
+                | Motion m -> "animateMotion", (m.Path |> Path.toAttributeString) + match m.CalculationMode with | Some(c) -> " " + Enum.GetName(typeof<CalculationMode>, c).ToLower() | None -> ""
+                | Color -> "animateColor", ""
+        Tag.create name 
+        |> Tag.addAttribute attribute
+        |> Tag.addAttribute (animation.Timing |> Timing.toString)
+
+    let toString animation = animation |> toTag |> Tag.toString
---
+++ SharpVG/Timing.fs
@@ -0,0 +1,87 @@
+﻿namespace SharpVG
+
+open System
+
+type Restart =
+    | Always
+    | WhenNotActive
+    | Never
+
+type DurationValue =
+    | Duration of TimeSpan
+    | Media
+
+type RepeatCountValue =
+    | RepeatCount of float
+    | Indefinite
+
+type RepeatDurationValue =
+    | RepeatDuration of TimeSpan
+    | Indefinite
+
+type Repetition =
+    {
+        RepeatCount: RepeatCountValue
+        RepeatDuration: RepeatDurationValue option
+    }
+
+type FinalState = // fill
+    | Frozen // freeze
+    | Hidden // remove
+
+type Timing =
+    {
+        Begin: TimeSpan
+        Duration: DurationValue option
+        End: TimeSpan option
+        Minimum: TimeSpan option
+        Maximum: TimeSpan option
+        Restart: Restart option
+        Repetition: Repetition option
+        FinalState: FinalState option
+    }
+
+[<CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
+module Timing =
+    let toString timing =
+        let timeSpanToString (timeSpan:TimeSpan) =
+            timeSpan.ToString("hh:mm:ss")
+
+        let durationToString duration =
+            match duration with
+                | Duration(d) -> d |> timeSpanToString
+                | Media -> "media"
+
+        let repetitionToString repetition =
+            "repeatCount=" + (
+                Tag.quote(
+                    match repetition.RepeatCount with
+                        | RepeatCount c -> string c
+                        | RepeatCountValue.Indefinite -> "indefinite"
+            )) +
+            match repetition.RepeatDuration with
+                | Some d ->
+                    "repeatDuration=" + (
+                        Tag.quote(
+                            match d with
+                                | RepeatDuration c ->  c |> timeSpanToString
+                                | RepeatDurationValue.Indefinite -> "indefinite"
+                        )
+                    )
+                | None -> ""
+
+        let finalStateToString finalState =
+            match finalState with
+                | Frozen -> "freeze"
+                | Hidden -> "remove"
+
+        [
+            Some("begin=" + (timing.Begin |> timeSpanToString));
+            timing.Duration |> Option.map (fun d -> "dur=" + Tag.quote(d |> durationToString));
+            timing.End |> Option.map (fun e -> "end=" + Tag.quote(e |> timeSpanToString));
+            timing.Minimum |> Option.map (fun m -> "min=" + Tag.quote(m |> timeSpanToString));
+            timing.Maximum |> Option.map (fun m -> "max=" + Tag.quote(m |> timeSpanToString));
+            timing.Restart |> Option.map (fun r -> Enum.GetName(typeof<Restart>, r).ToLower() |> Tag.quote);
+            timing.Repetition |> Option.map (fun r -> r |> repetitionToString);
+            timing.FinalState |> Option.map (fun f -> "fill=" + Tag.quote(f |> finalStateToString));
+        ] |> List.choose id |> String.concat " "
