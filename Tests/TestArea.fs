07c0a9e7ba8812ea12ce29e6bc9a700499edc426
Christopher Nikkel <chris@immortalcreation.com>
2016-08-20 09:04:56 -0700

    Add tests for area

---
+++ Tests/TestArea.fs
@@ -0,0 +1,19 @@
+﻿namespace SharpVG.Tests
+
+namespace SharpVG.Tests
+open SharpVG
+open Xunit
+open FsCheck
+open FsCheck.Xunit
+open BasicChecks
+open Swensen.Unquote
+
+module TestArea =
+
+    [<Fact>]
+    let ``create area`` () =
+        test <| <@ (Area.create Length.empty Length.empty |> Area.toString) = "0,0" @>
+
+    [<Fact>]
+    let ``transform point to attribute`` () =
+        test <| <@ (Area.create Length.empty Length.empty |> Area.toAttributes |> List.map Attribute.toString |> String.concat " ") = "height=\"0\" width=\"0\"" @>
