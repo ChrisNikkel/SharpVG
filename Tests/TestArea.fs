14f0a316e3dbe59285b0f13f1fb32c5c31b9b953
Christopher Nikkel <chris@immortalcreation.com>
2016-08-28 20:03:19 -0700

    More area tests

--- Tests/TestArea.fs
+++ Tests/TestArea.fs
@@ -13,6 +13,22 @@ module TestArea =
         test <| <@ (Area.create Length.empty Length.empty |> Area.toString) = "0,0" @>
 
     [<Fact>]
+    let ``create area of floats`` () =
+        test <| <@ (Area.ofFloats (0.0,0.0) |> Area.toString) = "0,0" @>
+
+    [<Fact>]
+    let ``create area of ints`` () =
+        test <| <@ (Area.ofInts (0,0) |> Area.toString) = "0,0" @>
+
+    [<Fact>]
+    let ``create area from points`` () =
+        test <| <@ (Area.fromPoints Point.origin Point.origin |> Area.toString) = "0,0" @>
+
+    [<Fact>]
     let ``transform point to attribute`` () =
         test <| <@ (Area.create Length.empty Length.empty |> Area.toAttributes |> List.map Attribute.toString |> String.concat " ") = "height=\"0\" width=\"0\"" @>
+
+    [<Fact>]
+    let ``create full area`` () =
+        test <| <@ (Area.full |> Area.toString) = "100%,100%" @>
 
