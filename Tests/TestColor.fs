a509463f8d87ccb253a67f48a70897b6bc68e96b
Christopher Nikkel <chris@freedomvoice.com>
2017-08-20 14:05:32 -0700

    fix tests to match of format for colors

--- Tests/TestColor.fs
+++ Tests/TestColor.fs
@@ -9,20 +9,20 @@ module TestColor =
 
     [<Fact>]
     let ``create with name`` () =
-        Assert.Equal(Color.createWithName Colors.Snow |> Color.toString, "snow")
+        Assert.Equal(Color.ofName Colors.Snow |> Color.toString, "snow")
 
     [<Fact>]
     let ``create with small hex`` () =
-        Assert.Equal(Color.createWithSmallHex 0x123s |> Color.toString, "0x123")
+        Assert.Equal(Color.ofSmallHex 0x123s |> Color.toString, "0x123")
 
     [<Fact>]
     let ``create with hex`` () =
-        Assert.Equal(Color.createWithHex 0x112233 |> Color.toString, "0x112233")
+        Assert.Equal(Color.ofHex 0x112233 |> Color.toString, "0x112233")
 
     [<Fact>]
     let ``create with percents`` () =
-        Assert.Equal(Color.createWithPercents (10.0, 10.0, 10.0) |> Color.toString, "(10%,10%,10%)")
+        Assert.Equal(Color.ofPercents (10.0, 10.0, 10.0) |> Color.toString, "(10%,10%,10%)")
 
     [<Property>]
     let ``create with values`` (r, g, b) =
-        Assert.Equal(Color.createWithValues (r, g, b) |> Color.toString, sprintf "(%d,%d,%d)" r g b)
+        Assert.Equal(Color.ofValues (r, g, b) |> Color.toString, sprintf "(%d,%d,%d)" r g b)
