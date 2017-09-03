2dd2ce736771f58977721835dca1f772bec83152
Christopher Nikkel <chris@freedomvoice.com>
2017-09-03 16:41:07 -0700

    refactor bounce

--- Examples/Bounce/src/App.fs
+++ Examples/Bounce/src/App.fs
@@ -3,25 +3,30 @@ module Bounce
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
+open Fable.Import.Browser
 open SharpVG
 
-let init() =
-    let content = Browser.document.getElementById "content"
+let createRect p c o =
     let area = Area.ofInts (55, 50)
-    let rect1 = Rect.create (Point.ofInts (10, 10)) area
-    let rect2 = Rect.create (Point.ofInts (30, 30)) area
-    let style1 = Style.createWithFill (Color.ofValues (200uy, 0uy, 0uy))
-    let style2 =
-      Style.createWithFill (Color.ofValues (0uy, 0uy, 200uy))
-        |> Style.withOpacity 0.5
-    let element1 = Element.ofRect rect1 |> Element.withStyle style1
-    let element2 = Element.ofRect rect2 |> Element.withStyle style2
+    let style = Style.createWithFill c |> Style.withOpacity 0.5
+    let rect = Rect.create (Point.ofInts p) area
+    Element.ofRect rect |> Element.withStyle style
 
-    let svg =
-      [element1; element2]
-        |> Group.ofList
-        |> Svg.ofGroup
-        |> Svg.toString
+let render pos1 pos2 =
+  let rect1 = createRect pos1 <| Color.ofValues (200uy, 0uy, 0uy) <| 1.0
+  let rect2 = createRect pos2 <| Color.ofValues (0uy, 0uy, 200uy) <| 0.5
+  [rect1; rect2]
+    |> Group.ofList
+    |> Svg.ofGroup
+    |> Svg.toString
 
-    content.innerHTML <- svg
+let rec update position () =
+  let (pos1, pos2) = position
+  (Browser.document.getElementById "content").innerHTML <- render pos1 pos2
+
+let init() =
+    let position = ((10, 10), (30, 30))
+    update position ()
+
+
 init()
