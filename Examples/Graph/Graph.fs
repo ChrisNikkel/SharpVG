4c1429e084c0f69b5090176b0c8a59a379422755
Christopher Nikkel <chris@freedomvoice.com>
2018-10-06 09:57:48 -0700

    fix Grpah example on OSX

--- Examples/Graph/Graph.fs
+++ Examples/Graph/Graph.fs
@@ -7,7 +7,7 @@ open System
 [<EntryPoint>]
 let main argv =
 
-    let fileName = "Graph.html"
+    let fileName = getTemporaryFileName
 
     Plot.line [ for i in 0.0 .. 0.02 .. 2.0 * Math.PI -> (1000.0 * sin i, 1000.0 * cos i * sin i) ]
         |> Svg.ofPlot
