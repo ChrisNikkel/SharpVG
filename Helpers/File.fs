af20601efdbc5bcccb8445476dc0db010d79181b
Christopher Nikkel <chris@freedomvoice.com>
2018-10-06 11:38:11 -0700

    make opening the html file cross platform

--- Helpers/File.fs
+++ Helpers/File.fs
@@ -2,14 +2,24 @@
 
 open System.Diagnostics
 open System.IO
+open System.Runtime.InteropServices
 
 module File =
-    let getTemporaryFileName =
-        Path.GetTempFileName()
+    let getTemporaryFileNameWithExt ext =
+        Path.ChangeExtension(Path.GetTempFileName(), ext)
 
     let saveToFile name lines =
         File.WriteAllLines(name, [lines])
 
     let openFile (name:string) =
-        let info = new ProcessStartInfo("open", name)
+
+        let checkOS os = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(os);
+        let launchCommand =
+            match checkOS OSPlatform.Windows, checkOS OSPlatform.OSX, checkOS OSPlatform.Linux with
+                | true, _, _ -> "start"
+                | _, true, _ -> "open"
+                | _, _, true -> "xdg-open"
+                | _, _, _ -> failwith ("Unsupported OS version: " + System.Runtime.InteropServices.RuntimeInformation.OSDescription)
+
+        let info = new ProcessStartInfo(launchCommand, name)
         Process.Start(info) |> ignore
