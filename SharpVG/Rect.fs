addc3b33834a85224b553d4339ac0813a7b89d8e
Christopher Nikkel <chris@freedomvoice.com>
2018-12-22 20:21:51 -0800

    add rounded corners to rect

--- SharpVG/Rect.fs
+++ SharpVG/Rect.fs
@@ -4,19 +4,24 @@ type Rect =
     {
         UpperLeft: Point
         Size: Area
+        CornerRadius: Point option
     }
 with
     static member ToTag rect =
         Tag.create "rect"
         |> Tag.withAttributes (Point.toAttributes rect.UpperLeft)
         |> Tag.addAttributes (Area.toAttributes rect.Size)
+        |> Tag.addAttributes (match rect.CornerRadius with Some(cr) -> Point.toAttributesWithModifier "r" "" cr | _ -> [])
 
     override this.ToString() =
        this |> Rect.ToTag |> Tag.toString
 
 module Rect =
     let create upperLeft size =
-        { UpperLeft = upperLeft; Size = size }
+        { UpperLeft = upperLeft; Size = size; CornerRadius = None }
+
+    let withCornerRadius point rect =
+        { rect with CornerRadius = Some(point) }
 
     let toTag =
         Rect.ToTag
--- Tests/TestRect.fs
+++ Tests/TestRect.fs
@@ -8,3 +8,5 @@ module TestRect =
     [<Fact>]
     let ``create rect`` () =
         Assert.Equal("<rect x=\"0\" y=\"0\" width=\"100%\" height=\"100%\"/>", Rect.create Point.origin Area.full |> Rect.toString)
+    let ``create rect with rounded corners`` () =
+        Assert.Equal("<rect x=\"0\" y=\"0\" rx=\"0\" ry=\"0\" width=\"100%\" height=\"100%\"/>", Rect.create Point.origin Area.full |> Rect.withCornerRadius Point.origin |> Rect.toString)
