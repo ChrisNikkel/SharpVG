93c7471b552be8fdfb0978085f35e3993dc702ca
Christopher Nikkel <chris@freedomvoice.com>
2018-12-23 07:53:04 -0800

    add basic use

---
+++ SharpVG/Use.fs
@@ -0,0 +1,26 @@
+namespace SharpVG
+
+type Use =
+    {
+        Name : string
+        Position : Point
+        Size : Area option
+    }
+with
+    static member ToTag useObj =
+        Tag.create "use"
+        |> Tag.withAttributes (Point.toAttributes useObj.Position)
+        |> Tag.addAttributes (match useObj.Size with Some(s) -> Area.toAttributes s | _ -> [])
+    override this.ToString() =
+       this |> Use.ToTag |> Tag.toString
+
+module Use =
+
+    let create name position : Use =
+        { Name = name; Position = position; Size = None }
+
+    let toTag =
+        Use.ToTag
+
+    let toString (useObj : Use) =
+        useObj.ToString()
