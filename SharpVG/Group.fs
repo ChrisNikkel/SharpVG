8910de009b3b3fe2513d5fa7ca16b3e63fa58d10
Christopher Nikkel <chris@freedomvoice.com>
2018-12-23 17:00:01 -0800

    add badges to readme

--- SharpVG/Group.fs
+++ SharpVG/Group.fs
@@ -11,6 +11,8 @@ and GroupElement =
 and Body =
     seq<GroupElement>
 
+//TODO : implemnt static member ToString to allow it to be used in Svg again
+
 module Group =
     let empty =
         { Name = None; Body = Seq.empty<GroupElement>; Transforms = Seq.empty }
