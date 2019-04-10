aef441cc3a2e278748c16757efc6358f53944f0e
Johan Sivertsen <jovt@itu.dk>
2019-04-10 22:35:48 +0200

    update examples to run with dotnet run -p

--- Examples/Graph/Graph.fs
+++ Examples/Graph/Graph.fs
@@ -7,7 +7,7 @@ open System
 [<EntryPoint>]
 let main argv =
 
-    let fileName = getTemporaryFileNameWithExt "html"
+    let fileName = "Graph.html"
     let plot2 = Plot.plot[ for i in 0.0 .. 0.02 .. 2.0 * Math.PI -> (500.0 * cos i * sin i, 500.0 * sin i) ]
 
     Plot.plot [ for i in 0.0 .. 0.02 .. 2.0 * Math.PI -> (1000.0 * sin i, 1000.0 * cos i * sin i) ]
