64863a9d713bc7d8fd298973d5f076452d545d8b
Chris Nikkel <chris@immortalcreation.com>
2020-10-03 22:26:12 +0000

    add letter spacing and make font float

--- SharpVG/Text.fs
+++ SharpVG/Text.fs
@@ -11,8 +11,9 @@ type Text =
         Position: Point
         Body: string
         FontFamily: string option
-        FontSize: int option
+        FontSize: float option
         Anchor: TextAnchor option
+        LetterSpacing: float option
     }
 with
     static member ToTag text =
@@ -39,6 +40,12 @@ with
                         | Some(Inherit) -> [Attribute.createXML "text-anchor" "inherit"]
                         | None -> []
             )
+        |> Tag.addAttributes
+            (
+                    match text.LetterSpacing with
+                        | Some(letterSpacing) -> [Attribute.createXML "letter-spacing" (string letterSpacing)]
+                        | None -> []
+            )
         |> Tag.addBody text.Body
 
     override this.ToString() =
@@ -46,7 +53,7 @@ with
 
 module Text =
     let create position body =
-        { Position = position; Body = body; FontFamily = None; FontSize = None; Anchor = None }
+        { Position = position; Body = body; FontFamily = None; FontSize = None; Anchor = None; LetterSpacing = None; }
 
     let withFont family size text =
         { text with FontFamily = Option.ofObj family; FontSize = Some(size) }
@@ -59,6 +66,9 @@ module Text =
 
     let withAnchor anchor text =
         { text with Anchor = Some(anchor) }
+
+    let withLetterSpacing letterSpacing text =
+        { text with LetterSpacing = Some(letterSpacing) }
 
     let toTag =
         Text.ToTag
