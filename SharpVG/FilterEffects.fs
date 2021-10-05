7f528a5ec833fcb2e84d194dbe2a6c6fc0d9bafc
Chris Nikkel <chris@immortalcreation.com>
2021-10-04 19:50:42 -0700

    add partial tag implementation for filter effect

--- SharpVG/FilterEffects.fs
+++ SharpVG/FilterEffects.fs
@@ -17,6 +17,24 @@ type BlendMode =
     | Saturation
     | Color
     | Luminosity
+    override this.ToString() =
+        match this with
+            | Normal -> "Normal"
+            | Multiply -> "Multiply"
+            | Screen -> "Screen"
+            | Overlay -> "Overlay"
+            | Darken -> "Darken"
+            | Lighten -> "Lighten"
+            | ColorDodge -> "ColorDodge"
+            | ColorBurn -> "ColorBurn"
+            | HardLight -> "HardLight"
+            | SoftLight -> "SoftLight"
+            | Difference -> "Difference"
+            | Exclusion -> "Exclusion"
+            | Hue -> "Hue"
+            | Saturation -> "Saturation"
+            | Color -> "Color"
+            | Luminosity -> "Luminosity"
 
 type EdgeMode =
     | Duplicate
@@ -77,10 +95,18 @@ type FilterEffects =
     // TODO: Implement Turbulence
 with
     static member ToTag filterEffects =
-        invalidOp "Not Implemented"
+        match filterEffects with
+            | Blend (blend) -> Tag.create "feBlend" |> Tag.withAttribute (Attribute.createXML "mode" (blend.ToString()))
+            | ColorMatrix (colorMatrix) -> Tag.create "feColorMatrix" // TODO: Add attributes
+            | Composite (composite) -> Tag.create "feComposite" // TODO: Add attributes
+            | DiffuseLighting (diffuseLighting) -> Tag.create "feDiffuseLighting" // TODO: Add attributes
+            | Flood (flood) -> Tag.create "Flood" // TODO: Add attributes
+            | GaussianBlur (gaussianBlur) -> Tag.create "feGaussianBlur" // TODO: Add attributes
+            | Image (image) -> Tag.create "feImage" // TODO: Add attributes
+            | Offset (offset) -> Tag.create "feOffset" // TODO: Add attributes
 
     override this.ToString() =
-        invalidOp "Not Implemented"
+        this |> FilterEffects.ToTag |> Tag.toString
 
 module FilterEffects =
     let empty : string =
